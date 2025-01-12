920633da4e479c0ae2f8d2f21aa3333a08fb455e
Eridanus-ui <njigupaul22@gmail.com>
2025-01-12 19:29:30 +0300

    feat: add pages for Customers, Home, Orders, Products

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Customers() {
+  return (
+    <div className="text-white bg-white flex justify-center items-center">
+      <h1>Customer</h1>
+    </div>
+  );
+}
