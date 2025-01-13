91a950359d182056ade32874ce4a56e62100134e
Eridanus-ui <njigupaul22@gmail.com>
2025-01-13 20:40:56 +0300

    feat(context): add AdminContext provider

---
+++ src/context/AdminContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+const AdminContext = createContext();
+
+const AdminContextProvider = (props) => {
+  const value = {};
+
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
