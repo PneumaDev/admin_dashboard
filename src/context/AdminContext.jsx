740bb85e8445548523ed7b064636f646e4404e86
Eridanus-ui <njigupaul22@gmail.com>
2025-01-18 09:39:30 +0300

    feat(context): Add AdminContext for order management
    - Implemented order fetching functionality.
    - Integrated axios for API calls.
    - Added loading state and error handling.
    - Used react-hot-toast for notifications.
    - Secured API calls with adminToken.

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,9 +1,56 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
-const AdminContext = createContext();
+export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const value = {};
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [adminToken, setAdminToken] = useState(null);
+
+  const fetchAllOrders = async () => {
+    if (!adminToken) return;
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        `${backendUrl}/api/order/list`,
+        {},
+        { headers: { adminToken } }
+      );
+
+      if (response.data.success) {
+        setOrders(response.data.orders.reverse());
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error("Failed to fetch orders:", error);
+      toast.error("Failed to fetch orders.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const savedToken = localStorage.getItem("adminToken");
+    if (!adminToken && savedToken) {
+      setAdminToken(savedToken);
+    }
+  }, [adminToken]);
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [adminToken]);
+
+  const value = {
+    backendUrl,
+    orders,
+    loading,
+    adminToken,
+    fetchAllOrders,
+  };
 
   return (
     <AdminContext.Provider value={value}>
