feced146354ad7cf7e1cba5e525bfb442280f85d
Eridanus-ui <njigupaul22@gmail.com>
2025-01-19 15:45:57 +0300

    refactor(PrivateRoute): remove Navigate component
    - Simplified component logic.
    - Improved code readability.

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { AdminContext } from "../context/AdminContext";
 import Login from "../pages/Login";
 
