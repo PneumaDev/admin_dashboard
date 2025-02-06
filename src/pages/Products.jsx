559e055815529b75e4ac1c5e773b2ab111ef41ee
Eridanus-ui <njigupaul22@gmail.com>
2025-02-06 08:06:52 +0300

    ✨ feat: fetch products only if admin token exists and display info message if no products are found

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,24 +3,33 @@ import { Edit, Trash, Plus, Search } from "lucide-react";
 import Table from "../components/Table";
 import { AdminContext } from "../context/AdminContext";
 import ProductActions from "../components/ProductActions";
-import { initialState } from "../assets/assets";
+import InfoMessage from "../components/InfoComponent";
 
 export default function ProductsPage() {
   const [fetchingProducts, setFetchingProducts] = useState(true);
-  const [itemData, setItemData] = useState(initialState);
 
-  const { products, fetchProducts, navigate, productAction, action } =
+  const { products, fetchProducts, navigate, productAction, adminToken } =
     useContext(AdminContext);
 
   useEffect(() => {
-    setFetchingProducts(true);
-    if (products.length === 0) {
-      fetchProducts();
-    }
-    if (products.length > 0) {
+    const loadProducts = async () => {
+      setFetchingProducts(true);
+
+      if (!adminToken) return;
+
+      try {
+        await fetchProducts();
+      } finally {
+        setFetchingProducts(false);
+      }
+    };
+
+    if (products && products.length === 0) {
+      loadProducts();
+    } else {
       setFetchingProducts(false);
     }
-  }, [products]);
+  }, [adminToken, products]);
 
   const columnHeader = {
     sn: "SN",
@@ -30,18 +39,6 @@ export default function ProductsPage() {
     category: "Category",
     actions: "Actions",
   };
-
-  // const productAction = (action, item) => {
-  //   if (action === "add") {
-  //     setAction("add");
-  //     setItemData(initialState);
-  //     setOpenModal(true);
-  //   } else {
-  //     setAction("edit");
-  //     setItemData((prevData) => ({ ...prevData, ...item }));
-  //     setOpenModal(true);
-  //   }
-  // };
 
   if (fetchingProducts) {
     return (
@@ -61,85 +58,95 @@ export default function ProductsPage() {
               Products
             </h1>
             <div className="flex space-x-4">
-              <button className="bg-blue-600 text-white px-2 md:px-4 py-2 rounded-lg flex items-center hover:bg-blue-700 font-imprima">
-                <Search className="w-4 h-4 md:mr-2" />
-                <span className="hidden md:flex">Search Product</span>
-              </button>
+              {products && (
+                <button className="bg-blue-600 cursor-pointer text-white px-2 md:px-4 py-2 rounded-lg flex items-center hover:bg-blue-700 font-imprima">
+                  <Search className="w-4 h-4 md:mr-2" />
+                  <span className="hidden md:flex ">Search Product</span>
+                </button>
+              )}
               <button
                 onClick={() => productAction("add")}
-                className="bg-blue-600 text-white px-2 md:px-4 py-2 rounded-lg flex items-center hover:bg-blue-700 font-imprima"
+                className="bg-blue-600 text-white cursor-pointer px-2 md:px-4 py-2 rounded-lg flex items-center hover:bg-blue-700 font-imprima"
               >
                 <Plus className="w-4 h-4 md:mr-2" />
-                <span className="hidden md:flex">Add Product</span>
+                <span className="hidden md:flex ">Add Product</span>
               </button>
             </div>
           </div>
         </div>
 
-        {/* Products Table */}
-        <Table
-          columnHeader={columnHeader}
-          products={products}
-          renderRow={(itemData, index) => (
-            <tr
-              key={index}
-              className="hover: cursor-pointer hover:bg-[var(--border-color)]"
-              onClick={() => navigate(`/products/${itemData._id}`)}
-            >
-              <td className="px-6 py-4 whitespace-nowrap ">
-                <div className="text-sm font-medium font-muktaVaani text-[var(--text-color)] transition-standard">
-                  {index + 1}.
-                </div>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="text-sm font-medium font-muktaVaani text-[var(--text-color)] transition-standard">
-                  {itemData.name}
-                </div>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <span
-                  className={`px-2 inline-flex text-xs font-muktaVaani leading-5 font-semibold rounded-full ${
-                    itemData.quantity === 0
-                      ? "bg-red-100 text-red-800"
+        {products ? (
+          <Table
+            columnHeader={columnHeader}
+            products={products}
+            renderRow={(itemData, index) => (
+              <tr
+                key={index}
+                className="hover: cursor-pointer hover:bg-[var(--border-color)]"
+                onClick={() => navigate(`/products/${itemData._id}`)}
+              >
+                <td className="px-6 py-4 whitespace-nowrap ">
+                  <div className="text-sm font-medium font-muktaVaani text-[var(--text-color)] transition-standard">
+                    {index + 1}.
+                  </div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm font-medium font-muktaVaani text-[var(--text-color)] transition-standard">
+                    {itemData.name}
+                  </div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span
+                    className={`px-2 inline-flex text-xs font-muktaVaani leading-5 font-semibold rounded-full ${
+                      itemData.quantity === 0
+                        ? "bg-red-100 text-red-800"
+                        : itemData.quantity <= 10
+                        ? "bg-yellow-100 text-yellow-800"
+                        : "bg-green-100 text-green-800"
+                    }`}
+                  >
+                    {itemData.quantity === 0
+                      ? "Out of Stock"
                       : itemData.quantity <= 10
-                      ? "bg-yellow-100 text-yellow-800"
-                      : "bg-green-100 text-green-800"
-                  }`}
-                >
-                  {itemData.quantity === 0
-                    ? "Out of Stock"
-                    : itemData.quantity <= 10
-                    ? "Low Stock"
-                    : "In Stock"}
-                </span>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap font-muktaVaani text-sm text-[var(--text-color)] transition-standard">
-                {itemData.quantity}
-              </td>
+                      ? "Low Stock"
+                      : "In Stock"}
+                  </span>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap font-muktaVaani text-sm text-[var(--text-color)] transition-standard">
+                  {itemData.quantity}
+                </td>
 
-              <td className="px-6 py-4 whitespace-nowrap font-muktaVaani text-sm text-[var(--text-color)] transition-standard">
-                {itemData.category}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                <button
-                  className="text-blue-600 hover:text-blue-500 mr-4"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    productAction("edit", itemData);
-                  }}
-                >
-                  <Edit className="w-6 h-6" />
-                </button>
-                <button
-                  className="text-red-600 hover:text-red-500"
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  <Trash className="w-6 h-6" />
-                </button>
-              </td>
-            </tr>
-          )}
-        />
+                <td className="px-6 py-4 whitespace-nowrap font-muktaVaani text-sm text-[var(--text-color)] transition-standard">
+                  {itemData.category}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                  <button
+                    className="text-blue-600 cursor-pointer hover:text-blue-500 mr-4"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      productAction("edit", itemData);
+                    }}
+                  >
+                    <Edit className="w-6 h-6" />
+                  </button>
+                  <button
+                    className="text-red-600 cursor-pointer hover:text-red-500"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <Trash className="w-6 h-6" />
+                  </button>
+                </td>
+              </tr>
+            )}
+          />
+        ) : (
+          <div className="flex justify-center">
+            <InfoMessage
+              title={"No Products Found"}
+              message={"Please add some products!"}
+            />
+          </div>
+        )}
 
         <ProductActions />
       </div>
