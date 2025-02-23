3dcda488b283349f4bbf3e7f25636839684b3337
Eridanus-ui <njigupaul22@gmail.com>
2025-02-23 19:34:38 +0300

    ✨ feat: Add order status update functionality

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -7,11 +7,14 @@ import Spinner from "../components/Spinner";
 import axios from "axios";
 import Modal from "../components/Modal";
 import VerifyPayments from "../components/VerifyPayments";
+import { useRef } from "react";
+import { toast } from "react-hot-toast";
 
 const Order = () => {
   const [order, setOrder] = useState(null);
   const [loading, setLoading] = useState(true);
   const [openModal, setOpenModal] = useState(false);
+  const [status, setStatus] = useState("Pending");
   const { orderId } = useParams();
   const { orders, backendUrl, adminToken } = useContext(AdminContext);
 
@@ -32,8 +35,51 @@ const Order = () => {
       );
       if (response.data.success) {
         setOrder(response.data.order);
+        setStatus(response.data.order.status);
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message, { id: "error" });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleStatusChange = async (event) => {
+    setLoading(true);
+    const newStatus = event.target.value;
+
+    try {
+      const response = await axios.post(
+        `${backendUrl}/api/order/status`,
+        {
+          orderId,
+          status: newStatus,
+        },
+        { headers: { token: adminToken } }
+      );
+      if (response.data.success) {
+        setStatus(newStatus);
+      } else {
+        throw new Error("Failed to update status");
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (event) => {
+    toast.promise(
+      () => handleStatusChange(event),
+      {
+        loading: "Updating status...",
+        success: "Status updated successfully! 🎉",
+        error: (error) => error.message || "Failed to update status.",
+      },
+      { id: "Update" }
+    );
   };
 
   if (loading && !order) {
@@ -74,27 +120,73 @@ const Order = () => {
         <div className="p-4 space-y-4">
           {/* Order Status */}
           <div className="border-b pb-4 border-gray-400">
-            <div className="flex items-center gap-x-2 mb-4">
-              <span className="font-semibold text-[var(--text-color)] transition-standard font-yantramanav">
-                Status:
-              </span>
-              <span
-                className={`px-2 py-1 rounded-md text-black font-muktaVaani text-sm ${
-                  order.status === "Delivered"
-                    ? "bg-green-200 text-green-900 hover:bg-green-300"
-                    : order.status === "Shipped"
-                    ? "bg-blue-100 text-blue-800 hover:bg-blue-200"
-                    : order.status === "Packing"
-                    ? "bg-orange-100 text-orange-600 hover:bg-orange-200"
-                    : order.status === "Pending"
-                    ? "bg-orange-100 text-orange-800 hover:bg-orange-200"
-                    : order.status === "Confirmed"
-                    ? "bg-teal-100 text-teal-800 hover:bg-teal-200"
-                    : "bg-red-100 text-red-800 hover:bg-red-200"
-                }`}
-              >
-                {order.status}
-              </span>
+            <div className="flex justify-between">
+              {/* Status Display */}
+              <div className="flex gap-x-2 mb-4 items-center">
+                <span className="font-semibold text-[var(--text-color)] transition-standard font-yantramanav">
+                  Status:
+                </span>
+                <span
+                  className={`px-2 py-1 rounded-md text-black font-muktaVaani text-sm ${
+                    status === "Delivered"
+                      ? "bg-green-200 text-green-900 hover:bg-green-300"
+                      : status === "Shipped"
+                      ? "bg-cyan-300 text-blue-800 hover:bg-cyan-400"
+                      : status === "Packing"
+                      ? "bg-blue-300 text-black hover:bg-blue-400"
+                      : status === "Pending"
+                      ? "bg-orange-100 text-orange-800 hover:bg-orange-200"
+                      : status === "Confirmed"
+                      ? "bg-teal-100 text-teal-800 hover:bg-teal-200"
+                      : "bg-red-100 text-red-800 hover:bg-red-200"
+                  }`}
+                >
+                  {status}
+                </span>
+              </div>
+
+              {/* Status Update Select */}
+              <div>
+                <select
+                  disabled={loading}
+                  title="status"
+                  value={status}
+                  onChange={handleChange}
+                  className="px-1 py-0.5 cursor-pointer md:py-1 text-sm md:text-base bg-[var(--bg-color)] border rounded-md text-[var(--text-color)] font-yantramanav"
+                >
+                  <option
+                    disabled={order.payment === true}
+                    value="Pending"
+                    className="text-[var(--text-color)] bg-[var(--hover-bg)] cursor-pointer"
+                  >
+                    Pending
+                  </option>
+                  <option
+                    value="Confirmed"
+                    className="text-[var(--text-color)] bg-[var(--hover-bg)] cursor-pointer"
+                  >
+                    Confirmed
+                  </option>
+                  <option
+                    value="Packing"
+                    className="text-[var(--text-color)] bg-[var(--hover-bg)] cursor-pointer"
+                  >
+                    Packing
+                  </option>
+                  <option
+                    value="Shipped"
+                    className="text-[var(--text-color)] bg-[var(--hover-bg)] cursor-pointer"
+                  >
+                    Shipped
+                  </option>
+                  <option
+                    value="Delivered"
+                    className="text-[var(--text-color)] bg-[var(--hover-bg)] cursor-pointer"
+                  >
+                    Delivered
+                  </option>
+                </select>
+              </div>
             </div>
 
             <div className="flex flex-col md:flex-row md:justify-between md:m-8 bg-[var(--bg-sidecolor)] transition-standard rounded-md gap-y-10 md:p-10 p-5 shadow-lg">
